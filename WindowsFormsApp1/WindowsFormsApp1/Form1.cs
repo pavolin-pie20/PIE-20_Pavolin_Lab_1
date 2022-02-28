e02d20ba0f32b69e74844f79ebe1d69d95fdcd1a
Pavolin-pie20 <pavolin@rambler.ru>
2022-02-28 11:30:40 +0300

    Lab-1

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Double FirstValue, TwoValue, Result;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*------------------------Visible---------------------------*/
+            label4.Visible = true;
+            /*----------------------------------------------------------*/
+
+            /*--------------------Вывод ответа--------------------------*/
+            try //try-catch
+            {
+                FirstValue = Convert.ToDouble(textBox1.Text);
+                TwoValue = Convert.ToDouble(textBox2.Text);
+                Result = FirstValue + TwoValue;
+                textBox3.Text = Result.ToString();
+                label4.Text = button1.Text;
+            }
+            catch
+            {
+                DialogResult result = MessageBox.Show("Вы неправильноо ввели данные",
+                    "Ошибка",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information,
+                    MessageBoxDefaultButton.Button1,
+                    MessageBoxOptions.DefaultDesktopOnly);
+                if (result == DialogResult.OK)
+                {
+                    this.Close();
+                }
+
+            }
+            /*----------------------------------------------------------*/
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /*------------------------Visible---------------------------*/
+            label4.Visible = true;
+            /*----------------------------------------------------------*/
+
+            /*--------------------Вывод ответа--------------------------*/
+            try //try-catch
+            {
+                FirstValue = Convert.ToDouble(textBox1.Text);
+                TwoValue = Convert.ToDouble(textBox2.Text);
+                Result = FirstValue - TwoValue;
+                textBox3.Text = Result.ToString();
+                label4.Text = button2.Text;
+            }
+            catch
+            {
+                DialogResult result = MessageBox.Show("Вы неправильноо ввели данные",
+                    "Ошибка",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information,
+                    MessageBoxDefaultButton.Button1,
+                    MessageBoxOptions.DefaultDesktopOnly);
+                if (result == DialogResult.OK)
+                {
+                    this.Close();
+                }
+
+            }
+            /*----------------------------------------------------------*/
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*------------------------Visible---------------------------*/
+            label4.Visible = true;
+            /*----------------------------------------------------------*/
+
+            /*--------------------Вывод ответа--------------------------*/
+            try //try-catch
+            {
+                FirstValue = Convert.ToDouble(textBox1.Text);
+                TwoValue = Convert.ToDouble(textBox2.Text);
+                Result = FirstValue * TwoValue;
+                textBox3.Text = Result.ToString();
+                label4.Text = button3.Text;
+            }
+            catch
+            {
+                DialogResult result = MessageBox.Show("Вы неправильноо ввели данные",
+                    "Ошибка",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information,
+                    MessageBoxDefaultButton.Button1,
+                    MessageBoxOptions.DefaultDesktopOnly);
+                if (result == DialogResult.OK)
+                {
+                    this.Close();
+                }
+
+            }
+            /*----------------------------------------------------------*/
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            /*------------------------Visible---------------------------*/
+            label4.Visible = true;
+            /*----------------------------------------------------------*/
+
+            /*--------------------Вывод ответа--------------------------*/
+            try //try-catch
+            {
+                FirstValue = Convert.ToDouble(textBox1.Text);
+                TwoValue = Convert.ToDouble(textBox2.Text);
+                Result = FirstValue / TwoValue;
+                textBox3.Text = Result.ToString();
+                label4.Text = button4.Text;
+            }
+            catch
+            {
+                DialogResult result = MessageBox.Show("Вы неправильноо ввели данные",
+                    "Ошибка",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information,
+                    MessageBoxDefaultButton.Button1,
+                    MessageBoxOptions.DefaultDesktopOnly);
+                if (result == DialogResult.OK)
+                {
+                    this.Close();
+                }
+
+            }
+            /*----------------------------------------------------------*/
+        }
+    }
+}
